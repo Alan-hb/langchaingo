692f1d766bd591b58e6397c709297d8570e42be4
zhaohaibo <zhaohaibo@zhihu.com>
2024-08-11 18:44:18 +0800

    t

--- llms/openai/internal/openaiclient/chat.go
+++ llms/openai/internal/openaiclient/chat.go
@@ -411,6 +411,7 @@ func parseStreamingChatResponse(ctx context.Context, r *http.Response, payload *
 			var streamPayload StreamedChatResponsePayload
 			err := json.NewDecoder(bytes.NewReader([]byte(data))).Decode(&streamPayload)
 			if err != nil {
+				continue
 				streamPayload.Error = fmt.Errorf("error decoding streaming response: %w", err)
 				responseChan <- streamPayload
 				return
